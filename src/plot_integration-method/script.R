e9f7fd01472814612c9fd74eb32bbdf5fab48a83
Adam Howes <adamhowes@outlook.com>
2022-01-18 19:47:32 +0000

    Add report to generate Figure 4.1

---
+++ src/plot_integration-method/script.R
@@ -0,0 +1,25 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("plot_integration-method")
+# setwd("src/plot_integration-method")
+
+#' The number of points in each area
+L <- 10
+
+#' The number of areas
+n <- nrow(mw)
+
+#' The different integration strategies
+random <- sf::st_sample(mw, size = rep(L, n))
+hexagonal <- sf::st_sample(mw, size = rep(L, n), type = "hexagonal", exact = TRUE)
+regular <- sf::st_sample(mw, size = rep(L, n), type = "regular", exact = TRUE)
+
+pdf("integration-method.pdf", h = 4, w = 6.25)
+
+cowplot::plot_grid(
+  plot_samples(random, title = "Random"),
+  plot_samples(hexagonal, title = "Hexagonal"),
+  plot_samples(regular, title = "Regular"),
+  ncol = 3
+)
+
+dev.off()
