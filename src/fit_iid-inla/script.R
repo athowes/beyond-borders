e7dffb4c0ebcbbc267a1497f03aea014db8f0589
Adam Howes <adamhowes@outlook.com>
2022-01-20 17:00:53 +0000

    Report to run bsae::iid_inla!

---
+++ src/fit_iid-inla/script.R
@@ -0,0 +1,22 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("fit_iid-inla")
+# setwd("src/fit_iid-inla")
+
+geometries <- c("grid", "civ", "tex")
+sim_models <- c("iid", "icar", "ik")
+
+#' Geometry and simulation model
+pars <- expand.grid(
+  "geometry" = geometries,
+  "sim_model" = sim_models
+)
+
+#' Function to run bsae::iid_inla
+run_models <- function(geometry, sim_model) {
+  data <- readRDS(paste0("depends/data_", sim_model, "_", geometry, ".rds"))
+  fits <- lapply(data, function(x) bsae::iid_inla(x$sf))
+  saveRDS(paste0("fits_", sim_model, "_", geometry, ".rds"))
+}
+
+# Run models and save for each row of pars
+purrr::pmap_df(pars, run_models)
