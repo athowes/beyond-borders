86d616be1cec8980995ff8ff58830fad78268257
Adam Howes <adamhowes@outlook.com>
2022-01-20 19:24:25 +0000

    Add templates for all of the other models types

---
+++ src/fit_besag-inla/script.R
@@ -0,0 +1,3 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("fit_besag-inla")
+# setwd("src/fit_besag-inla")
---
+++ src/fit_fck-inla/script.R
@@ -0,0 +1,3 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("fit_fck-inla")
+# setwd("src/fit_fck-inla")
