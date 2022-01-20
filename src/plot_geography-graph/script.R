fa9310af700b3e3bc27907f4c6d87b72df47d6e7
Adam Howes <adamhowes@outlook.com>
2022-01-20 16:57:31 +0000

    Geometry is a better word than geography (less connotations)

--- src/plot_geography-graph/script.R
+++
@@ -1,9 +0,0 @@
-#' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("plot_geography-graph")
-# setwd("src/plot_geography-graph")
-
-pdf("geography-graph.pdf", h = 2.5, w = 6.25)
-
-bsae::plot_graph(mw, add_geography = TRUE)
-
-dev.off()
---
+++ src/plot_geometry-graph/script.R
@@ -0,0 +1,9 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("plot_geometry-graph")
+# setwd("src/plot_geometry-graph")
+
+pdf("geometry-graph.pdf", h = 2.5, w = 6.25)
+
+bsae::plot_graph(mw, add_geography = TRUE)
+
+dev.off()
