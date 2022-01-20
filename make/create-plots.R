8f1c7e96de3ed4e3d1f60a12c6909d9a1fcf4f9e
Adam Howes <adamhowes@outlook.com>
2022-01-20 18:41:24 +0000

    Add simulation-geometries (and update a name)

--- make/create-plots.R
+++ make/create-plots.R
@@ -1,6 +1,7 @@
 source("make/utils.R")
 
 run_commit_push("plot_areal-data")
-run_commit_push("plot_geography-graph")
+run_commit_push("plot_geometry-graph")
 run_commit_push("plot-tikz_maup")
 run_commit_push("plot_integration-method")
+run_commit_push("plot_simulation-geometries")
