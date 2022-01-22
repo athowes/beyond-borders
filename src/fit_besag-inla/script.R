a6f35d7b9f351acebad7aaa5995ec796aef8fa49
Adam Howes <adamhowes@outlook.com>
2022-01-22 18:57:20 +0000

    Steps forward with the model fitting code

--- src/fit_besag-inla/script.R
+++ src/fit_besag-inla/script.R
@@ -1,3 +1,22 @@
 #' Uncomment and run the two line below to resume development of this script
 # orderly::orderly_develop_start("fit_besag-inla")
 # setwd("src/fit_besag-inla")
+
+geometries <- c("grid", "civ", "tex")
+sim_models <- c("iid", "icar", "ik")
+
+#' Geometry and simulation model
+pars <- expand.grid(
+  "geometry" = geometries,
+  "sim_model" = sim_models
+)
+
+#' Function to run bsae::besag_inla
+run_models <- function(geometry, sim_model) {
+  data <- readRDS(paste0("depends/data_", sim_model, "_", geometry, ".rds"))
+  fits <- lapply(data, function(x) bsae::besag_inla(x$sf))
+  saveRDS(paste0("fits_", sim_model, "_", geometry, ".rds"))
+}
+
+# Run models and save for each row of pars
+purrr::pmap_df(pars, run_models)
