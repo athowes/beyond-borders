8f2a6f35867d7f1609f0e7a26681fcf4598e2d7f
Adam Howes <adamhowes@outlook.com>
2022-01-24 21:51:38 +0000

    Bring intercept assessment up to speed with rho assessment (don't need data here because it's just always -2, but might want to start adding this into the simulation data in future)

--- src/assess_intercept-marginal/script.R
+++ src/assess_intercept-marginal/script.R
@@ -4,4 +4,46 @@
 
 geometries <- c("grid", "civ", "tex")
 sim_models <- c("iid", "icar", "ik")
+
+#' Currently we're testing with just the one constant model, but in future it will seven different models
 inf_models <- c("constant_inla")
+# inf_models <- c("constant_inla", "besag_inla", "bym2_inla", "fck_inla", "fik_inla", "ck_stan", "ik_stan")
+
+pars <- expand.grid(
+  "geometry" = geometries,
+  "sim_model" = sim_models,
+  "inf_model" = inf_models
+)
+
+#' Function to process fits
+process_fits <- function(geometry, sim_model, inf_model) {
+
+  message(
+    "Beginning assessment of intercept posterior marginal of the ", inf_model,
+    " model fit to ", sim_model, " data on the ", geometry, " geometry."
+  )
+
+  #' The fitted models
+  fits_loc <- paste0("depends/fits_", sim_model, "_", geometry, "_", inf_model, ".rds")
+  fits <- readRDS(fits_loc)
+
+  #' Assess the intercept marginal
+  df <- lapply(fits, assess_marginal_intercept, intercept = -2) %>%
+    bsae::list_to_df() %>%
+    #' Add columns for meta data
+    mutate(
+      geometry = geometry,
+      sim_model = sim_model,
+      inf_model = inf_model,
+      .before = replicate
+    )
+
+  message("intercept parameter assessment complete.")
+
+
+}
+
+#' Eventually this will be iterated over geometries, sim_models and inf_models
+df <- purrr::pmap_df(pars, process_fits)
+
+saveRDS(df, "df.rds")
