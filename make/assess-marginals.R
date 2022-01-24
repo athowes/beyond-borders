41086aba83f3a62052561ae478d997fc6f108b82
Adam Howes <adamhowes@outlook.com>
2022-01-24 23:34:51 +0000

    Post-processing of intercept marginal assessment

--- make/assess-marginals.R
+++ make/assess-marginals.R
@@ -1,4 +1,5 @@
 source("make/utils.R")
 
 run_commit_push("assess_rho-marginals")
+run_commit_push("assess_intercept-marginal")
 run_commit_push("assess_time-taken") #' (It's not really a marginal)
--- src/plot_metric-boxplots/script.R
+++ src/plot_metric-boxplots/script.R
@@ -3,11 +3,24 @@
 # setwd("src/plot_metric-boxplots")
 
 df_rho <- readRDS("depends/df_rho.rds")
+df_intercept <- readRDS("depends/df_intercept")
 
-#' CRPS boxplot
+#' CRPS boxplot for rho
 pdf("crps-boxplot-rho.pdf", h = 4, w = 6.25)
 
 df_rho %>%
+  update_naming() %>%
+  boxplot(
+    metric = "crps",
+    y_lab = "CRPS"
+  )
+
+dev.off()
+
+#' CRPS boxplot for the intercept
+pdf("crps-boxplot-intercept.pdf", h = 4, w = 6.25)
+
+df_intercept %>%
   update_naming() %>%
   boxplot(
     metric = "crps",
