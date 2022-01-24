97372c4601141db7af933e66ba92e61c7330fda3
Adam Howes <adamhowes@outlook.com>
2022-01-24 11:33:11 +0000

    These are really repetative (bad programming practise) but hopefully if there are individual model specific issues will be easier to diagnose and fix. Seems like similar trade off to "country-specific" task versus single aaa_task

--- src/fit_ik-stan/script.R
+++ src/fit_ik-stan/script.R
@@ -1,3 +1,22 @@
 #' Uncomment and run the two line below to resume development of this script
 # orderly::orderly_develop_start("fit_ik-stan")
 # setwd("src/fit_ik-stan")
+
+geometries <- c("grid", "civ", "tex")
+sim_models <- c("iid", "icar", "ik")
+
+#' Geometry and simulation model
+pars <- expand.grid(
+  "geometry" = geometries,
+  "sim_model" = sim_models
+)
+
+#' Function to run bsae::ik_stan
+run_models <- function(geometry, sim_model) {
+  data <- readRDS(paste0("depends/data_", sim_model, "_", geometry, ".rds"))
+  fits <- lapply(data, function(x) bsae::ik_stan(x$sf))
+  saveRDS(fits, file = paste0("fits_", sim_model, "_", geometry, ".rds"))
+}
+
+# Run models and save for each row of pars
+purrr::pmap_df(pars, run_models)
