d7171ee5ee3853d6b7d69ca72d51bb4eff371a0d
Adam Howes <adamhowes@outlook.com>
2022-01-26 19:42:43 +0000

    Attempting to complete #4

---
+++ make/pull-surveys.R
@@ -0,0 +1,14 @@
+#' The four surveys required are: GF AGYW countries
+#' * MWI2015DHS
+#' * ZWE2015DHS
+#' * TZA2012AIS
+#' * CIV2012DHS
+
+#' Currently getting this:
+#' Error in report_db_open_existing(con, config) :
+#'   custom fields 'requester', 'author' not present in existing database
+
+orderly::orderly_pull_archive("mwi_data_survey-indicators", remote = "malawi")
+orderly::orderly_pull_archive("zwe_data_survey", remote = "naomi2")
+orderly::orderly_pull_archive("tza_data_survey", remote = "naomi2")
+orderly::orderly_pull_archive("civ_data_survey", remote = "naomi2")
