28ddd29688ffa255ad9c971891eca245afde7653
Adam Howes <adamhowes@outlook.com>
2022-02-09 12:45:22 +0000

    Template for #13

---
+++ src/draw_sf-geometries/script.R
@@ -0,0 +1,13 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("draw_sf-geometries")
+# setwd("src/draw_sf-geometries")
+
+geometry_i <- NULL
+geometry_ii <- NULL
+geometry_iii <- NULL
+geometry_iv <- NULL
+
+saveRDS(geometry_i, "geometry-i.rds")
+saveRDS(geometry_ii, "geometry-ii.rds")
+saveRDS(geometry_iii, "geometry-iii.rds")
+saveRDS(geometry_iv, "geometry-iv.rds")
