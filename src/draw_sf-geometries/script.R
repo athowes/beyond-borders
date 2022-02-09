550eccf2e286be7dc21f6d56cd7ce740abe50001
Adam Howes <adamhowes@outlook.com>
2022-02-09 21:32:11 +0000

    Numeric are better than roman #15

--- src/draw_sf-geometries/script.R
+++ src/draw_sf-geometries/script.R
@@ -2,17 +2,17 @@
 # orderly::orderly_develop_start("draw_sf-geometries")
 # setwd("src/draw_sf-geometries")
 
-#' Geometry (i)
+#' Geometry 1
 area1 <- matrix(c(0, 0, 1, 0, 1, 1, 0, 1, 0, 0), ncol = 2, byrow = TRUE)
 area2 <- area1
 area2[, 1] <- area2[, 1] + 1
 area3 <- area1
 area3[, 1] <- area3[, 1] + 2
-geometry_i <- st_polygon(list(area1, area2, area3))
+geometry_1 <- st_polygon(list(area1, area2, area3))
 
-saveRDS(geometry_i, "geometry-i.rds")
+saveRDS(geometry_1, "geometry-1.rds")
 
-#' Geometry (ii)
+#' Geometry 2
 p <- st_point(c(0, 0))
 radius1 <- st_buffer(p, dist = 1)
 radius2 <- st_buffer(p, dist = 2)
@@ -20,44 +20,44 @@ radius3 <- st_buffer(p, dist = 3)
 area1 <- radius1
 area2 <- st_difference(radius2, radius1)
 area3 <- st_difference(radius3, radius2)
-geometry_ii <- st_multipolygon(list(area1, area2, area3))
+geometry_2 <- st_multipolygon(list(area1, area2, area3))
 
-saveRDS(geometry_ii, "geometry-ii.rds")
+saveRDS(geometry_2, "geometry-2.rds")
 
-#' Geometry (iii)
+#' Geometry 3
 area1 <- matrix(c(0, -0.5, 0, 0.5, 1, 1, 1, -1, 0, -0.5), ncol = 2, byrow = TRUE)
 area2 <- matrix(c(1, -1, 1, 1, 2, 1.5, 2, -1.5, 1, -1), ncol = 2, byrow = TRUE)
 area3 <- matrix(c(2, -1.5, 2, 1.5, 3, 2, 3, -2, 2, -1.5), ncol = 2, byrow = TRUE)
-geometry_iii <- st_polygon(list(area1, area2, area3))
+geometry_3 <- st_polygon(list(area1, area2, area3))
 
-saveRDS(geometry_iii, "geometry-iii.rds")
+saveRDS(geometry_3, "geometry-3.rds")
 
-#' Geometry (iv)
+#' Geometry 4
 area1 <- matrix(c(0, 0, 0.5, 0, 0.5, 1, 0, 1, 0, 0), ncol = 2, byrow = TRUE)
 area2 <- matrix(c(0.5, 0, 1.5, 0, 1.5, 1, 0.5, 1, 0.5, 0), ncol = 2, byrow = TRUE)
 area3 <- matrix(c(1.5, 0, 3, 0, 3, 1, 1.5, 1, 1.5, 0), ncol = 2, byrow = TRUE)
-geometry_iv <- st_polygon(list(area1, area2, area3))
+geometry_4 <- st_polygon(list(area1, area2, area3))
 
-saveRDS(geometry_iv, "geometry-iv.rds")
+saveRDS(geometry_4, "geometry-4.rds")
 
 pdf("geometries.pdf", h = 4, w = 6.25)
 
 cowplot::plot_grid(
   ggplot() +
-    geom_sf(data = geometry_i) +
-    #labs(title = "(i)") +
+    geom_sf(data = geometry_1) +
+    #labs(title = "1") +
     theme_void(),
   ggplot() +
-    geom_sf(data = geometry_ii) +
-    #labs(title = "(ii)") +
+    geom_sf(data = geometry_2) +
+    #labs(title = "2") +
     theme_void(),
   ggplot() +
-    geom_sf(data = geometry_iii) +
-    #labs(title = "(iii)") +
+    geom_sf(data = geometry_3) +
+    #labs(title = "3") +
     theme_void(),
   ggplot() +
-    geom_sf(data = geometry_iv) +
-    #labs(title = "(iv)") +
+    geom_sf(data = geometry_4) +
+    #labs(title = "4") +
     theme_void(),
   ncol = 2
 )
