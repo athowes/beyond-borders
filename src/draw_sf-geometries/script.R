0b6bf6c6864bc47e2f60b551c246cf3d6b3b3102
Adam Howes <adamhowes@outlook.com>
2022-02-09 13:06:20 +0000

    First geometry complete #13

--- src/draw_sf-geometries/script.R
+++ src/draw_sf-geometries/script.R
@@ -2,12 +2,33 @@
 # orderly::orderly_develop_start("draw_sf-geometries")
 # setwd("src/draw_sf-geometries")
 
-geometry_i <- NULL
-geometry_ii <- NULL
-geometry_iii <- NULL
-geometry_iv <- NULL
+#' Geometry (i)
+area1 <- matrix(c(0, 0, 1, 0, 1, 1, 0, 1, 0, 0), ncol = 2, byrow = TRUE)
+area2 <- area1
+area2[, 1] <- area2[, 1] + 1
+area3 <- area1
+area3[, 1] <- area3[, 1] + 2
+geometry_i <- st_polygon(list(area1, area2, area3))
+
+pdf("geometry-i.pdf", h = 4, w = 6.25)
+
+ggplot() +
+  geom_sf(data = geometry_i) +
+  labs(title = "(i)") +
+  theme_void()
+
+dev.off()
 
 saveRDS(geometry_i, "geometry-i.rds")
+
+#' Geometry (ii)
+geometry_ii <- NULL
 saveRDS(geometry_ii, "geometry-ii.rds")
+
+#' Geometry (iii)
+geometry_iii <- NULL
 saveRDS(geometry_iii, "geometry-iii.rds")
+
+#' Geometry (iv)
+geometry_iv <- NULL
 saveRDS(geometry_iv, "geometry-iv.rds")
