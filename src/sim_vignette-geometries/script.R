25bfbc2523c0f7d4015f85a2b8363c753e87b179
Adam Howes <adamhowes@outlook.com>
2022-02-10 12:43:37 +0000

    Matching vignette to realistic formatting of data #16

--- src/sim_vignette-geometries/script.R
+++ src/sim_vignette-geometries/script.R
@@ -11,21 +11,21 @@ geometry_4 <- readRDS("depends/geometry-4.rds")
 plot(geometry_1)
 
 #' Geometry 1
-sim_iid(geometry_1, nsim) %>% saveRDS("1_data_iid.rds")
-sim_icar(geometry_1, nsim) %>% saveRDS("1_data_icar.rds")
-sim_ik(geometry_1, L = 100, nsim, l = 2.5) %>% saveRDS("1_data_ik.rds")
+sim_iid(geometry_1, nsim) %>% saveRDS("data_iid_1.rds")
+sim_icar(geometry_1, nsim) %>% saveRDS("data_icar_1.rds")
+sim_ik(geometry_1, L = 100, nsim, l = 2.5) %>% saveRDS("data_ik_1.rds")
 
 #' Geometry 2
-sim_iid(geometry_2, nsim) %>% saveRDS("2_data_iid.rds")
-sim_icar(geometry_2, nsim) %>% saveRDS("2_data_icar.rds")
-sim_ik(geometry_2, L = 100, nsim, l = 2.5) %>% saveRDS("2_data_ik.rds")
+sim_iid(geometry_2, nsim) %>% saveRDS("data_iid_2.rds")
+sim_icar(geometry_2, nsim) %>% saveRDS("data_icar_2.rds")
+sim_ik(geometry_2, L = 100, nsim, l = 2.5) %>% saveRDS("data_ik_2.rds")
 
 #' Geometry 3
-sim_iid(geometry_3, nsim) %>% saveRDS("3_data_iid.rds")
-sim_icar(geometry_3, nsim) %>% saveRDS("3_data_icar.rds")
-sim_ik(geometry_3, L = 100, nsim, l = 2.5) %>% saveRDS("3_data_ik.rds")
+sim_iid(geometry_3, nsim) %>% saveRDS("data_iid_3.rds")
+sim_icar(geometry_3, nsim) %>% saveRDS("data_icar_3.rds")
+sim_ik(geometry_3, L = 100, nsim, l = 2.5) %>% saveRDS("data_ik_3.rds")
 
 #' Geometry 4
-sim_iid(geometry_4, nsim) %>% saveRDS("4_data_iid.rds")
-sim_icar(geometry_4, nsim) %>% saveRDS("4_data_icar.rds")
-sim_ik(geometry_4, L = 100, nsim, l = 2.5) %>% saveRDS("4_data_ik.rds")
+sim_iid(geometry_4, nsim) %>% saveRDS("data_iid_4.rds")
+sim_icar(geometry_4, nsim) %>% saveRDS("data_icar_4.rds")
+sim_ik(geometry_4, L = 100, nsim, l = 2.5) %>% saveRDS("data_ik_4.rds")
