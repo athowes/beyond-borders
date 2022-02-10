124f02ca6bc646b4891c1152de04afdc746fce56
Adam Howes <adamhowes@outlook.com>
2022-02-10 12:27:33 +0000

    Update make file #16

--- make/simulate-data.R
+++ make/simulate-data.R
@@ -3,6 +3,5 @@ source("make/utils.R")
 run_commit_push("draw_sf-geometries")
 run_commit_push("sim_vignette-geometries")
 
-run_commit_push("sim_civ")
-run_commit_push("sim_grid")
-run_commit_push("sim_tex")
+run_commit_push("sim_realistic-geometries")
+
