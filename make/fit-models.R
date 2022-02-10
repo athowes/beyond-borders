f2cae45c0a3222602ecbc84fa6f2f1aba321a5e1
Adam Howes <adamhowes@outlook.com>
2022-02-10 21:52:02 +0000

    Fitting both vignette and realistic #17

--- make/fit-models.R
+++ make/fit-models.R
@@ -1,17 +1,25 @@
 source("make/utils.R")
 
-#' Vignette
-run_commit_push("fit_constant-inla", param = list(realistic = FALSE))
-run_commit_push("fit_iid-inla", param = list(realistic = FALSE))
-run_commit_push("fit_besag-inla", param = list(realistic = FALSE))
-run_commit_push("fit_bym2-inla", param = list(realistic = FALSE))
-run_commit_push("fit_fck-inla", param = list(realistic = FALSE))
-run_commit_push("fit_fik-inla", param = list(realistic = FALSE))
+#' Vignette and realistic together
+run_commit_push("fit_constant-inla")
+run_commit_push("fit_iid-inla")
+run_commit_push("fit_besag-inla")
+run_commit_push("fit_bym2-inla")
+run_commit_push("fit_fck-inla")
+run_commit_push("fit_fik-inla")
 
-#' Realistic
-run_commit_push("fit_constant-inla", param = list(vignette = FALSE))
-run_commit_push("fit_iid-inla", param = list(vignette = FALSE))
-run_commit_push("fit_besag-inla", param = list(vignette = FALSE))
-run_commit_push("fit_bym2-inla", param = list(vignette = FALSE))
-run_commit_push("fit_fck-inla", param = list(vignette = FALSE))
-run_commit_push("fit_fik-inla", param = list(vignette = FALSE))
+#' #' Vignette
+#' run_commit_push("fit_constant-inla", param = list(realistic = FALSE))
+#' run_commit_push("fit_iid-inla", param = list(realistic = FALSE))
+#' run_commit_push("fit_besag-inla", param = list(realistic = FALSE))
+#' run_commit_push("fit_bym2-inla", param = list(realistic = FALSE))
+#' run_commit_push("fit_fck-inla", param = list(realistic = FALSE))
+#' run_commit_push("fit_fik-inla", param = list(realistic = FALSE))
+#'
+#' #' Realistic
+#' run_commit_push("fit_constant-inla", param = list(vignette = FALSE))
+#' run_commit_push("fit_iid-inla", param = list(vignette = FALSE))
+#' run_commit_push("fit_besag-inla", param = list(vignette = FALSE))
+#' run_commit_push("fit_bym2-inla", param = list(vignette = FALSE))
+#' run_commit_push("fit_fck-inla", param = list(vignette = FALSE))
+#' run_commit_push("fit_fik-inla", param = list(vignette = FALSE))
