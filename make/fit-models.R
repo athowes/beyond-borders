9f64af0ff9049994faa0e499d78d26b326031330
Adam Howes <adamhowes@outlook.com>
2022-02-10 17:18:53 +0000

    Add to make file #17

--- make/fit-models.R
+++ make/fit-models.R
@@ -1,10 +1,17 @@
 source("make/utils.R")
 
-run_commit_push("fit_constant-inla")
-run_commit_push("fit_iid-inla")
-run_commit_push("fit_besag-inla")
-run_commit_push("fit_bym2-inla") #' n = 20 took 30 minutes
-run_commit_push("fit_fck-inla")
-run_commit_push("fit_fik-inla")
-# run_commit_push("fit_ck-stan")
-# run_commit_push("fit_ik-stan")
+#' Vignette
+run_commit_push("fit_constant-inla", param = list(realistic = FALSE))
+run_commit_push("fit_iid-inla", param = list(realistic = FALSE)
+run_commit_push("fit_besag-inla", param = list(realistic = FALSE)
+run_commit_push("fit_bym2-inla", param = list(realistic = FALSE)
+run_commit_push("fit_fck-inla", param = list(realistic = FALSE)
+run_commit_push("fit_fik-inla", param = list(realistic = FALSE)
+
+#' Realistic
+run_commit_push("fit_constant-inla", param = list(vignette = FALSE))
+run_commit_push("fit_iid-inla", param = list(vignette = FALSE))
+run_commit_push("fit_besag-inla", param = list(vignette = FALSE))
+run_commit_push("fit_bym2-inla", param = list(vignette = FALSE))
+run_commit_push("fit_fck-inla", param = list(vignette = FALSE))
+run_commit_push("fit_fik-inla", param = list(vignette = FALSE))
--- make/utils.R
+++ make/utils.R
@@ -1,13 +1,14 @@
 #' Run and optionally commit or push a collection of reports.
 #'
 #' @param `reports` A vector of report names.
+#' @param `param` Paramters passed to all reports.
 #' @param `commit` Should the reports be commited? Defaults to `TRUE`.
 #' @param `push` Should the reports be pushed to remote? Defaults to `FALSE`.
-run_commit_push <- function(reports, commit = TRUE, push = FALSE) {
+run_commit_push <- function(reports, param, commit = TRUE, push = FALSE) {
   sapply(
     reports,
     function(report) {
-      id <- orderly::orderly_run(report)
+      id <- orderly::orderly_run(report, parameters = param)
       if(commit) { orderly::orderly_commit(id) }
       if(push) { orderly::orderly_push_archive(report) }
     }
