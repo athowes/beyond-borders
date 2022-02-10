b7aaee95a9682efb7ddece83e1315e162ee56fb0
Adam Howes <adamhowes@outlook.com>
2022-02-10 17:32:59 +0000

    Close brackets ) #17

--- make/fit-models.R
+++ make/fit-models.R
@@ -2,11 +2,11 @@ source("make/utils.R")
 
 #' Vignette
 run_commit_push("fit_constant-inla", param = list(realistic = FALSE))
-run_commit_push("fit_iid-inla", param = list(realistic = FALSE)
-run_commit_push("fit_besag-inla", param = list(realistic = FALSE)
-run_commit_push("fit_bym2-inla", param = list(realistic = FALSE)
-run_commit_push("fit_fck-inla", param = list(realistic = FALSE)
-run_commit_push("fit_fik-inla", param = list(realistic = FALSE)
+run_commit_push("fit_iid-inla", param = list(realistic = FALSE))
+run_commit_push("fit_besag-inla", param = list(realistic = FALSE))
+run_commit_push("fit_bym2-inla", param = list(realistic = FALSE))
+run_commit_push("fit_fck-inla", param = list(realistic = FALSE))
+run_commit_push("fit_fik-inla", param = list(realistic = FALSE))
 
 #' Realistic
 run_commit_push("fit_constant-inla", param = list(vignette = FALSE))
