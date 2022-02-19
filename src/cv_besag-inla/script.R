2e2984f33bdc1885f645584686a44ac7f9ca2496
Adam Howes <adamhowes@outlook.com>
2022-02-19 11:50:56 +0000

    HIV study, all other models #3

--- src/cv_besag-inla/script.R
+++ src/cv_besag-inla/script.R
@@ -1,3 +1,43 @@
 #' Uncomment and run the two line below to resume development of this script
 # orderly::orderly_develop_start("cv_besag-inla")
 # setwd("src/cv_besag-inla")
+
+surveys <- c("civ2017phia", "mwi2016phia")
+# surveys <- c("civ2017phia", "mwi2016phia", "tza2017phia", "zwe2016phia")
+types <- c("loo")
+# types <- c("loo", "sloo")
+
+pars <- expand.grid(
+  "survey" = surveys,
+  "type" = types
+)
+
+#' Function to run bsae::besag_inla for each cross-validation fold
+run_cv_models <- function(survey, type) {
+  sf <- read_csv(paste0("depends/", survey, ".csv"))
+  training_sets <- create_folds(sf, type = toupper(type))
+
+  print(paste0("Begin ", toupper(type), " cross-valdiation of ", toupper(survey)))
+  pb <- progress_estimated(nrow(sf)) #' Initialise progress bar
+
+  training_sets <- lapply(training_sets, function(training_set) {
+    training_set$fit <- bsae::besag_inla(training_set$data)
+    pb$tick()$print()
+    return(training_set)
+  })
+
+  saveRDS(training_sets, file = paste0("fits_", substr(survey, 1, 3), "_", tolower(type), ".rds"))
+}
+
+#' Run models and save for each row of pars
+purrr::pmap_df(pars, run_cv_models)
+
+#' Function to run bsae::besag_inla for the whole dataset
+#' This will be useful for comparing R-INLA and Stan inbuilt model comparison metrics to manual CV
+run_models <- function(survey, type) {
+  sf <- read_csv(paste0("depends/", survey, ".csv"))
+  fit <- bsae::besag_inla(sf)
+  saveRDS(fit, file = paste0("fit_", substr(survey, 1, 3), ".rds"))
+}
+
+purrr::pmap_df(expand.grid("survey" = surveys), run_models)
