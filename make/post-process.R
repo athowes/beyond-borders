a9ac1e6c53c0273bbcd3597c648c9276b130c8b7
Adam Howes <adamhowes@outlook.com>
2022-02-22 14:55:21 +0000

    More plots in make file

--- make/post-process.R
+++ make/post-process.R
@@ -1,6 +1,13 @@
 source("make/utils.R")
 
+#' Simulation study
 run_commit_push("process_metric-tables")
 run_commit_push("plot_metric-maps")
 run_commit_push("plot_metric-boxplots")
 run_commit_push("plot_coverage")
+run_commit_push("plot_lengthscale-recovery")
+
+#' HIV study
+run_commit_push("process_hiv-metric-tables")
+run_commit_push("plot_hiv-metric-maps")
+run_commit_push("process_compare-to-manual-cv") #' Probably supplementary material
