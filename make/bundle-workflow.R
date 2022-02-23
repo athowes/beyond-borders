4222ea0bcbb8f11ed87c94a04fbc545a598df009
Adam Howes <adamhowes@outlook.com>
2022-02-23 21:21:58 +0000

    Testing bundle workflow #24

---
+++ make/bundle-workflow.R
@@ -0,0 +1,55 @@
+#' A. On local machine:
+#' 1. Create bundle
+#' 2. Upload to sharepoint somewhere with spud
+#' B. On windows VM:
+#' 1. Pull bundle
+#' 2. Run bundle
+#' 3. Upload to sharepoint
+#' C. On local machine
+#' 1. Pull bundle
+#' 2. Import into archive
+
+#' A1.
+path_bundles <- "bundle-input"
+bundle <- orderly::orderly_bundle_pack(path_bundles, "fit_ik-stan")
+
+#' A2.
+spud <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
+folder <- spud$folder("HIVInferenceGroup-WP", "Shared Documents/orderly/areal-comparison/bundle-input", verify = TRUE)
+folder$upload(path = bundle$path)
+
+recent_bundle <- folder$list() %>%
+  filter(created == max(created))
+
+#' B1.
+folder$download(
+  path = recent_bundle$name,
+  dest = paste0("bundle-input/", recent_bundle$name)
+)
+
+#' B2.
+orderly_packages <- yaml::read_yaml(file.path("src/fit_ik-stan/orderly.yml"))$packages
+packages <- list(loaded = c("orderly", orderly_packages))
+
+config <- didehpc::didehpc_config(
+  workdir = path_bundles,
+  credentials = "ath19",
+  cluster = "fi--didemrchnb"
+  # "fi--dideclusthn"
+  # "fi--didemrchnb"
+)
+
+src <- conan::conan_sources("athowes/bsae")
+
+ctx <- context::context_save(
+  "context",
+  packages = packages,
+  package_sources = src
+)
+
+obj <- didehpc::queue_didehpc(ctx, config = config)
+
+t <- obj$enqueue(orderly::orderly_bundle_run(recent_bundle$name, "bundle-output"))
+
+t$status()
+t$result()
