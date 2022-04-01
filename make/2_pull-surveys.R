bd70079f03b76737a8f3fbc0e37f2d337e9bd016
Adam Howes <adamhowes@outlook.com>
2022-04-01 16:27:57 +0100

    Run 2_pull-surveys.R

--- make/2_pull-surveys.R
+++ make/2_pull-surveys.R
@@ -6,15 +6,15 @@ source("make/utils.R")
 #' * tza2017phia
 #' * zwe2016phia
 
-orderly::orderly_pull_archive("civ_data_survey", remote = "naomi2")
-orderly::orderly_pull_archive("mwi_data_survey-indicators", remote = "malawi")
-orderly::orderly_pull_archive("tza_data_survey", remote = "naomi2")
-orderly::orderly_pull_archive("zwe_data_survey", remote = "naomi2")
+orderly::orderly_pull_archive("civ_data_survey", remote = "naomi2") #' [x]
+orderly::orderly_pull_archive("mwi_data_survey-indicators", remote = "malawi") #' [x]
+orderly::orderly_pull_archive("tza_data_survey", remote = "naomi2") #' [x]
+orderly::orderly_pull_archive("zwe_data_survey", remote = "naomi2") #' [x]
 
-run_commit_push("process_hiv-surveys", push = FALSE)
+run_commit_push("2_process_hiv-surveys", push = FALSE) #' [x]
 
 #' Pull the area files too
-orderly::orderly_pull_archive("civ_data_areas", remote = "naomi2")
-orderly::orderly_pull_archive("mwi_data_areas", remote = "naomi2")
-orderly::orderly_pull_archive("tza_data_areas", remote = "naomi2")
-orderly::orderly_pull_archive("zwe_data_areas", remote = "naomi2")
+orderly::orderly_pull_archive("civ_data_areas", remote = "naomi2") #' [x]
+orderly::orderly_pull_archive("mwi_data_areas", remote = "naomi2") #' [x]
+orderly::orderly_pull_archive("tza_data_areas", remote = "naomi2") #' [x]
+orderly::orderly_pull_archive("zwe_data_areas", remote = "naomi2") #' [x]
