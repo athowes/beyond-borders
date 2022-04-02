fbb7fa834482390575cf7fc2fbaf8f97d9e438e7
Adam Howes <adamhowes@outlook.com>
2022-04-02 13:38:53 +0100

    Run 2 up to CV and assessment of first four models

--- make/2_cv-models.R
+++ make/2_cv-models.R
@@ -1,12 +1,12 @@
 source("make/utils.R")
 
-run_commit_push("cv_constant-inla")
-run_commit_push("cv_iid-inla")
-run_commit_push("cv_besag-inla")
-run_commit_push("cv_bym2-inla")
-run_commit_push("cv_fck-inla")
-run_commit_push("cv_fik-inla")
-run_commit_push("cv_ck-stan")
-run_commit_push("cv_ik-stan")
+run_commit_push("2_cv_constant-inla") #' [x]
+run_commit_push("2_cv_iid-inla") #' [x]
+run_commit_push("2_cv_besag-inla") #' [x]
+run_commit_push("2_cv_bym2-inla") #' [x]
+run_commit_push("2_cv_fck-inla") #' [/] See issue 35
+run_commit_push("2_cv_fik-inla") #' [/] See issue 36
+run_commit_push("2_cv_ck-stan") #' []
+run_commit_push("2_cv_ik-stan") #' []
 
-run_commit_push("assess_cv")
+run_commit_push("2_assess_cv") #' [x]
