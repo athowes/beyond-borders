89d213aabbb6600d05924760c42d6d7bf75a5508
athowes <adamhowes@outlook.com>
2022-08-02 23:23:52 -0400

    Start thinking about how to compare manual CV to direct CV

--- src/2_process_compare-to-manual-cv/script.R
+++ src/2_process_compare-to-manual-cv/script.R
@@ -1,3 +1,10 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("process_compare-to-manual-cv")
-# setwd("src/process_compare-to-manual-cv")
+# orderly::orderly_develop_start("2_process_compare-to-manual-cv")
+# setwd("src/2_process_compare-to-manual-cv")
+
+manual <- readRDS("depends/manual.rds")
+direct <- readRDS("depends/direct.rds")
+
+#' Need to get CPO in manual CV to compare to direct from R-INLA
+manual
+direct
