47c4ac13a8939d92b8aba9097471515707c53ed9
athowes <adamhowes@outlook.com>
2022-12-12 13:50:42 +0000

    One CV script to rule them all

--- src/2_cv/script.R
+++ src/2_cv/script.R
@@ -1,10 +1,10 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("2_cv_constant-inla")
-# setwd("src/2_cv_constant-inla")
+# orderly::orderly_develop_start("2_cv", parameters = list(f = "constant_inla"))
+# setwd("src/2_cv")
 
 surveys <- list("civ2017phia", "mwi2016phia", "tza2017phia", "zwe2016phia")
 types <- list("loo", "sloo")
-fs <- list(bsae::constant_inla)
+fs <- list(get(f, envir = asNamespace("bsae")))
 pars <- expand.grid("survey" = surveys, "f" = fs)
 cv_pars <- expand.grid("survey" = surveys, "type" = types, "f" = fs)
 
