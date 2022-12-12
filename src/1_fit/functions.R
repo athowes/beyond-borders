761ed24edd840716fcc9e484b9c16543b8dbd0b9
athowes <adamhowes@outlook.com>
2022-12-12 14:45:07 +0000

    Just use one fit script here too

---
+++ src/1_fit/functions.R
@@ -0,0 +1,28 @@
+orderly_shush <- function() {
+  lapply(
+    expand.grid("sim_model" = sim_models, "geometry" = c(vignette_geometries, realistic_geometries)) %>%
+      as.data.frame() %>%
+      mutate(file = paste0("fits_", sim_model, "_", geometry, ".rds")) %>%
+      pull(file),
+    function(file) saveRDS(NULL, file)
+  )
+}
+
+run_models <- function(geometry, sim_model, f) {
+  pb$tick()$print()
+
+  #' This model can't handle the concentric circles case!
+  if(f %in% c(fck_inla, ck_stan) & geometry == "2") return(NULL)
+
+  data <- readRDS(paste0("depends/data_", sim_model, "_", geometry, ".rds"))
+  fits <- lapply(data, function(x) {
+    fit <- f(x$sf)
+    if(class(fit) == "inla") {
+      samples <- INLA::inla.posterior.sample(n = 1000, fit)
+      fit[["samples"]] <- samples
+      class(fit) <- "inlax"
+    }
+    return(fit)
+  })
+  saveRDS(fits, file = paste0("fits_", sim_model, "_", geometry, ".rds"))
+}
---
+++ src/1_fit/script.R
@@ -0,0 +1,22 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("1_fit", parameters = list(f = "constant_inla"))
+# setwd("src/1_fit")
+
+geometries <- c()
+vignette_geometries <- as.character(1:4)
+realistic_geometries <- c("grid", "civ", "tex")
+if(vignette) geometries <- c(geometries, vignette_geometries)
+if(realistic) geometries <- c(geometries, realistic_geometries)
+if(length(geometries) == 0) stop("Either vignette or realistic must be TRUE")
+
+sim_models <- c("iid", "icar", "ik")
+fs <- list(get(f, envir = asNamespace("bsae")))
+
+pars <- expand.grid("geometry" = geometries, "sim_model" = sim_models, "f" = fs)
+
+#' Prevent orderly complaining by generating all possible simulations
+orderly_shush()
+
+# Run models and save for each row of pars
+pb <- progress_estimated(nrow(pars))
+purrr::pmap(pars, run_models)
