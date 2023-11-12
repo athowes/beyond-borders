9b7b39f5674aeabea5644bbe2df0120f768ad35d
athowes <adamthowes@gmail.com>
2023-11-12 12:35:18 +0000

    Merging fit and assess

--- make/1_all.R
+++ make/1_all.R
@@ -8,7 +8,7 @@ run_commit_push("1_sim_realistic-geometries") #' [x]
 #' 2. Create plots
 run_commit_push("1_plot_simulation-geometries") #' [x]
 
-#' 3. Fit models
+#' 3. Fit models and assess marginals
 
 #' Vignette and realistic together
 id <- orderly::orderly_run("1_fit", param = list(f = "constant_aghq")) #' [ ]
@@ -35,18 +35,12 @@ orderly::orderly_commit(id)
 id <- orderly::orderly_run("1_fit", param = list(f = "ik_aghq"))       #' [ ]
 orderly::orderly_commit(id)
 
-#' 4. Assess marginals
-run_commit_push("1_assess_rho-marginals")        #' [ ]
-run_commit_push("1_assess_intercept-marginal")   #' [ ]
-run_commit_push("1_assess_lengthscale-marginal") #' [ ]
-run_commit_push("1_assess_time-taken")           #' [ ]
-
-#' 5. Post-process
+#' 4. Post-process
 run_commit_push("1_process_metric-tables")     #' [ ]
 run_commit_push("1_plot_metric-maps")          #' [ ]
 run_commit_push("1_plot_metric-boxplots")      #' [ ]
 run_commit_push("1_plot_coverage")             #' [ ]
 run_commit_push("1_plot_lengthscale-recovery") #' [ ]
 
-#' 6. Checks
+#' 5. Checks
 run_commit_push("1_checks") #' [ ]
