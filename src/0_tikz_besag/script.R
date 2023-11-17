3117d7816ebd62fbabd6aa42dae8d62f9b0ebfff
athowes <adamthowes@gmail.com>
2023-11-17 12:05:02 +0000

    Shorten names

--- src/0_tikz_besag/script.R
+++ src/0_tikz_besag/script.R
@@ -1,6 +1,6 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("0_plot-tikz_besag")
-# setwd("src/0_plot-tikz_besag")
+# orderly::orderly_develop_start("0_tikz_besag")
+# setwd("src/0_tikz_besag")
 
 tools::texi2dvi("besag.tex", pdf = TRUE, clean = TRUE)
 knitr::plot_crop("besag.pdf")
