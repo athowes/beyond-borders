0138444805a0dc7629569959920e92ca6b118423
athowes <adamthowes@gmail.com>
2023-11-18 18:55:06 +0000

    Renamings

--- make/2_all.R
+++ make/2_all.R
@@ -1,6 +1,6 @@
 source("make/utils.R")
 
-run_commit_push("2_checks")
+run_commit_push("2_checks") #' [ ]
 
 #' 1. Pull surveys
 
@@ -10,33 +10,28 @@ run_commit_push("2_checks")
 #' * tza2017phia
 #' * zwe2016phia
 
-orderly::orderly_pull_archive("civ_data_survey", remote = "naomi2")            #' [ ]
-orderly::orderly_pull_archive("mwi_data_survey-indicators", remote = "malawi") #' [ ]
-orderly::orderly_pull_archive("tza_data_survey", remote = "naomi2")            #' [ ]
-orderly::orderly_pull_archive("zwe_data_survey", remote = "naomi2")            #' [ ]
+orderly::orderly_pull_archive("civ_data_survey", remote = "naomi2")            #' [x]
+orderly::orderly_pull_archive("mwi_data_survey-indicators", remote = "malawi") #' [x]
+orderly::orderly_pull_archive("tza_data_survey", remote = "naomi2")            #' [x]
+orderly::orderly_pull_archive("zwe_data_survey", remote = "naomi2")            #' [x]
+orderly::orderly_pull_archive("civ_data_areas", remote = "naomi2")             #' [x]
+orderly::orderly_pull_archive("mwi_data_areas", remote = "naomi2")             #' [x]
+orderly::orderly_pull_archive("tza_data_areas", remote = "naomi2")             #' [x]
+orderly::orderly_pull_archive("zwe_data_areas", remote = "naomi2")             #' [x]
 
-run_commit_push("2_process_hiv-surveys", push = FALSE) #' [x]
-
-#' Pull the area files too
-orderly::orderly_pull_archive("civ_data_areas", remote = "naomi2") #' [ ]
-orderly::orderly_pull_archive("mwi_data_areas", remote = "naomi2") #' [ ]
-orderly::orderly_pull_archive("tza_data_areas", remote = "naomi2") #' [ ]
-orderly::orderly_pull_archive("zwe_data_areas", remote = "naomi2") #' [ ]
+run_commit_push("2_process_surveys", push = FALSE) #' [x]
 
 #' 2. Cross-validate models
-run_commit_push("2_cv", param = list(f = "constant_inla")) #' [ ]
-run_commit_push("2_cv", param = list(f = "iid_inla"))      #' [ ]
-run_commit_push("2_cv", param = list(f = "besag_inla"))    #' [ ]
-run_commit_push("2_cv", param = list(f = "bym2_inla"))     #' [ ]
-run_commit_push("2_cv", param = list(f = "fck_inla"))      #' [ ]
-run_commit_push("2_cv", param = list(f = "fik_inla"))      #' [ ]
-run_commit_push("2_cv", param = list(f = "ck_stan"))       #' [ ]
-run_commit_push("2_cv", param = list(f = "ik_stan"))       #' [ ]
-
-run_commit_push("2_assess_cv") #' [ ]
+run_commit_push("2_cv", param = list(f = "constant_aghq")) #' [ ]
+run_commit_push("2_cv", param = list(f = "iid_aghq"))      #' [ ]
+run_commit_push("2_cv", param = list(f = "besag_aghq"))    #' [ ]
+run_commit_push("2_cv", param = list(f = "bym2_aghq"))     #' [ ]
+run_commit_push("2_cv", param = list(f = "fck_aghq"))      #' [ ]
+run_commit_push("2_cv", param = list(f = "fik_aghq"))      #' [ ]
+run_commit_push("2_cv", param = list(f = "ck_aghq"))       #' [ ]
+run_commit_push("2_cv", param = list(f = "ik_aghq"))       #' [ ]
 
 #' 3. Post-process
-run_commit_push("2_process_metric-tables")        #' [ ]
-run_commit_push("2_plot_metric-maps")             #' [ ]
-run_commit_push("2_plot_prev-ladder")             #' [ ]
-run_commit_push("2_process_compare-to-manual-cv") #' [ ]
+run_commit_push("2_process_tables") #' [ ]
+run_commit_push("2_plot_maps")      #' [ ]
+run_commit_push("2_plot_ladder")    #' [ ]
