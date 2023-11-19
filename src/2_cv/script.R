717c77d0fc0ded95fd15c63f78463fef98fc4715
athowes <adamthowes@gmail.com>
2023-11-19 17:58:07 +0000

    Start reformatting HIV survey study

--- src/2_cv/script.R
+++ src/2_cv/script.R
@@ -1,12 +1,47 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("2_cv", parameters = list(f = "constant_inla"))
+# orderly::orderly_develop_start("2_cv", parameters = list(f = "iid_aghq"))
 # setwd("src/2_cv")
 
 surveys <- list("civ2017phia", "mwi2016phia", "tza2017phia", "zwe2016phia")
-types <- list("loo", "sloo")
 fs <- list(get(f, envir = asNamespace("arealutils")))
-pars <- expand.grid("survey" = surveys, "f" = fs)
-cv_pars <- expand.grid("survey" = surveys, "type" = types, "f" = fs)
 
-purrr::pmap(cv_pars, arealutils:::run_cv_models)
-purrr::pmap(pars, arealutils:::run_models)
+#' Cross-validation
+# run_cv <- function(survey, type, inf_function) {
+#   message("Begin ", toupper(type), " cross-valdiation of ", f, " to the survey ", toupper(survey))
+#   sf <- st_read(paste0("depends/", survey, ".geojson"))
+#   training_sets <- bsae::create_folds(sf, type = toupper(type))
+#
+#   training_sets <- lapply(training_sets, function(x) {
+#     x$fit <- inf_function(x$data)
+#     return(x)
+#   })
+#
+#   return(training_sets)
+# }
+#
+# types <- list("loo", "sloo")
+# cv_pars <- expand.grid("survey" = surveys, "type" = types, "inf_function" = fs)
+# cv <- purrr::pmap(cv_pars, safely(run_cv))
+
+saveRDS(NULL, file = "cv.rds")
+
+#' Information criteria and regular fitting
+run_ic <- function(survey, type, inf_function) {
+  message("Begin fitting of ", f, " to the survey ", toupper(survey))
+  sf <- st_read(paste0("depends/", survey, ".geojson"))
+  fit <- inf_function(sf)
+  samples <- aghq::sample_marginal(fit, M = 1000)
+  x_samples <- samples$samps
+  rho_samples <- plogis(x_samples + x_samples["beta_0", ])[-1, ]
+  summaries <- function(x) c(mean(x), quantile(x, c(0.5, 0.025, 0.975)))
+  rho_summaries <- data.frame(t(apply(rho_samples, 1, summaries)), row.names = NULL)
+  names(rho_summaries) <- c("mean", "mode", "lower", "upper")
+  result <- bind_cols(select(sf, survey_id, area_name, y, n_obs), rho_summaries)
+  result$inf_model <- f
+  return(result)
+}
+
+ic_pars <- expand.grid("survey" = surveys, "inf_function" = fs)
+ic <- purrr::pmap(ic_pars, safely(run_ic))
+
+saveRDS(ic, file = "ic.rds")
--- src/2_plot_ladder/script.R
+++ src/2_plot_ladder/script.R
@@ -1,80 +1,19 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("2_plot_prev-ladder")
-# setwd("src/2_plot_prev-ladder")
+# orderly::orderly_develop_start("2_plot_ladder")
+# setwd("src/2_plot_ladder")
 
-df <- readRDS("depends/df.rds")
-direct <- readRDS("depends/manual.rds")
+ic <- readRDS("depends/ic_iid.rds")
 
-surveys <- c("civ2017phia", "mwi2016phia", "tza2017phia", "zwe2016phia")
+lapply(ic, function(x) {
 
-pdf("prev-ladder.pdf", h = 8, w = 6.25)
+  if(is.null(x$result)) return(NULL)
 
-lapply(surveys, function(x) {
-  df %>%
-    filter(survey_id == toupper(x)) %>%
-    mutate(inf_model = recode_factor(inf_model,
-      "raw" = "Raw",
-      "constant_inla" = "Constant",
-      "iid_inla" = "IID",
-      "besag_inla" = "Besag",
-      "bym2_inla" = "BYM2",
-      "fck_inla" = "FCK",
-      "ck_stan" = "CK",
-      "fik_inla" = "FIK",
-      "ik_stan" = "IK")
-    ) %>%
-    group_by(area_id) %>%
-    mutate(average_estimate = mean(estimate)) %>%
-    ungroup() %>%
-    mutate(area_name = fct_reorder(area_name, average_estimate)) %>%
-    ggplot(aes(x = area_name, y = estimate, ymin = ci_lower, ymax = ci_upper, group = inf_model, color = inf_model)) +
+  ggplot(x$result, aes(x = forcats::fct_reorder(area_name, mean), y = mean, ymin = lower, ymax = upper)) +
     geom_pointrange(position = position_dodge(width = 0.6), alpha = 0.8) +
-    labs(x = "", y = "Posterior HIV prevalence estimate", col = "Inferential model") +
-    theme_minimal() +
-    scale_color_manual(values = multi.utils::cbpalette()) +
     coord_flip() +
     theme_minimal() +
-    theme(
-      legend.position = "bottom",
-      legend.title = element_text(size = 9),
-      legend.text = element_text(size = 9)
-    )
+    scale_y_continuous(labels = scales::percent) +
+    labs(x = "Area name", y = "Prevalence estimate")
+
+  ggsave(paste0("ladder-", tolower(x$result$survey_id[1]), ".png"), h = 8, w = 6.25, bg = "white")
 })
-
-dev.off()
-
-pdf("crps-ladder.pdf", h = 8, w = 6.25)
-
-lapply(surveys, function(x) {
-  manual %>%
-    filter(survey == x) %>%
-    mutate(inf_model = recode_factor(inf_model,
-                                     "raw" = "Raw",
-                                     "constant_inla" = "Constant",
-                                     "iid_inla" = "IID",
-                                     "besag_inla" = "Besag",
-                                     "bym2_inla" = "BYM2",
-                                     "fck_inla" = "FCK",
-                                     "ck_stan" = "CK",
-                                     "fik_inla" = "FIK",
-                                     "ik_stan" = "IK")
-    ) %>%
-    group_by(id) %>%
-    mutate(average_crps = mean(crps)) %>%
-    ungroup() %>%
-    mutate(id = fct_reorder(as.factor(id), average_crps)) %>%
-    ggplot(aes(x = id, y = crps, group = inf_model, color = inf_model)) +
-    geom_point(position = position_dodge(width = 0.6), alpha = 0.8) +
-    labs(x = "", y = "CRPS", col = "Inferential model") +
-    theme_minimal() +
-    scale_color_manual(values = multi.utils::cbpalette()) +
-    coord_flip() +
-    theme_minimal() +
-    theme(
-      legend.position = "bottom",
-      legend.title = element_text(size = 9),
-      legend.text = element_text(size = 9)
-    )
-})
-
-dev.off()
