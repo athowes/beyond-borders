e2f2697c2f3bff4d91968612c6077fdfd3cffa96
athowes <adamthowes@gmail.com>
2023-11-22 09:43:52 +0000

    Template for aghq against tmbstan comparison for appendix

---
+++ src/1_check_aghq-nuts/script.R
@@ -0,0 +1,13 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("1_check_aghq-nuts")
+# setwd("src/1_check_aghq-nuts")
+
+#' Check that aghq and tmbstan produce similar answers
+
+#' IID
+#' Besag
+#' BYM2
+#' FCK
+#' FIK
+#' CK
+#' IK
