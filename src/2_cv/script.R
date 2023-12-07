2440af5c44e044598a643f7c4677d209a9c294e2
athowes <adamthowes@gmail.com>
2023-12-07 20:22:45 +0000

    Fix IC component of CV

--- src/2_cv/script.R
+++ src/2_cv/script.R
@@ -52,14 +52,13 @@ saveRDS(cv, file = "cv.rds")
 run_ic <- function(survey, type, inf_function) {
   message("Begin fitting of ", f, " to the survey ", toupper(survey))
   sf <- readRDS(paste0("depends/", survey, ".rds"))
-  fit <- inf_function(sf)
+  capture.output(fit <- inf_function(sf, ii_mis = NULL))
   samples <- aghq::sample_marginal(fit, M = 1000)
   x_samples <- samples$samps
   u_samples <- x_samples[rownames(x_samples) == "u", ]
   beta_0_samples <- x_samples[1, ]
   rho_samples <- plogis(u_samples + beta_0_samples)
-  summaries <- function(x) c(mean(x), quantile(x, c(0.5, 0.025, 0.975)))
-  rho_summaries <- data.frame(t(apply(rho_samples, 1, summaries)), row.names = NULL)
+  rho_summaries <- data.frame(t(apply(rho_samples, 1, function(x) c(mean(x), quantile(x, c(0.5, 0.025, 0.975))))), row.names = NULL)
   names(rho_summaries) <- c("mean", "mode", "lower", "upper")
   result <- bind_cols(select(sf, survey_id, area_name, y, n_obs), rho_summaries)
   result$inf_model <- f
