7543ae1188de5302d44acd78a1c4cc306b23590d
athowes <adamthowes@gmail.com>
2023-12-07 19:26:37 +0000

    Don't need these!

--- src/2_check_assess-code/script.R
+++
@@ -1,5 +0,0 @@
-#' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("2_check_assess-code")
-# setwd("src/2_check_assess-code")
-
-rmarkdown::render("assess.Rmd")
