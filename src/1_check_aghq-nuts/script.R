ece632ba9e81d8b2e325e340ab0c554728a5c94d
athowes <adamthowes@gmail.com>
2023-12-12 15:58:56 +0000

    Improve AGHQ NUTS check

--- src/1_check_aghq-nuts/script.R
+++ src/1_check_aghq-nuts/script.R
@@ -5,7 +5,7 @@
 #' Check that aghq and tmbstan produce similar answers
 
 data <- readRDS("depends/data_iid_grid.rds")
-sf <- data[[1]]$sf
+sf <- data[[2]]$sf
 
 plot_mean_sd <- function(fit_aghq, fit_nuts, subtitle) {
   aghq_summary <- summary(fit_aghq, max_print = 100, M = 2500)
@@ -34,18 +34,17 @@ plot_mean_sd <- function(fit_aghq, fit_nuts, subtitle) {
     mutate(
       diff = NUTS - AGHQ,
       diff_pct = diff / (sign(NUTS) * pmax(0.25, abs(NUTS))),
-      type = forcats::fct_recode(type, "Mean" = "mean", "SD" = "sd")
+      type = forcats::fct_recode(type, "Posterior mean" = "mean", "Posterior SD" = "sd")
     )
 
   ggplot(df, aes(x = AGHQ, y = diff_pct)) +
     geom_point(alpha = 0.5, shape = 1) +
     facet_wrap(~ type, scales = "free_x") +
+    geom_abline(intercept = 0, slope = 0, col = "#E69F00", linetype = "dashed") +
     scale_y_continuous(labels = scales::percent) +
-    labs(x = "AGHQ estimate", y = "Percentage error against NUTS", subtitle = subtitle) +
+    labs(x = "Estimate", y = "Percentage difference to NUTS", subtitle = subtitle) +
     theme_minimal()
 }
-
-time_df <- data.frame("inf_model" = c(), "aghq" = c(), "tmbstan" = c())
 
 get_time <- function(t) {
   as.numeric(t$toc - t$tic)
@@ -53,22 +52,25 @@ get_time <- function(t) {
 
 #' IID
 tictoc::tic()
-fit_aghq <- arealutils::iid_aghq(sf, k = 3)
+fit_aghq <- arealutils::iid_aghq(sf, k = 3, ii = NULL)
 time_aghq <- tictoc::toc()
 
 tictoc::tic()
-fit_nuts <- arealutils::iid_tmbstan(sf, nsim_warm = 500, nsim_iter = 1000, chains = 4)
+fit_nuts <- arealutils::iid_tmbstan(sf, nsim_warm = 500, nsim_iter = 1000, chains = 4, ii = NULL)
 time_nuts <- tictoc::toc()
 
 plot_mean_sd(fit_aghq, fit_nuts, "IID model")
 ggsave("iid-aghq-nuts.png", h = 3.5, w = 6.25)
 
-time_df <- rbind(c("IID", get_time(time_aghq), get_time(time_nuts))) %>%
-  as.data.frame()
+time_df <- rbind(c("IID", get_time(time_aghq), get_time(time_nuts))) %>% as.data.frame()
+names(time_df) <- c("inf_model", "aghq", "tmbstan")
+
+diagnostics_df <- rbind(c("IID", min(head(summary(fit_nuts)$summary[, "n_eff"], -1)), max(head(bayesplot::rhat(fit_nuts), -1)))) %>% as.data.frame()
+names(diagnostics_df) <- c("inf_model", "min_ess", "max_rhat")
 
 #' Besag
 tictoc::tic()
-fit_aghq <- arealutils::besag_aghq(sf, k = 3)
+fit_aghq <- arealutils::besag_aghq(sf, k = 3, ii = NULL)
 time_aghq <- tictoc::toc()
 
 tictoc::tic()
@@ -79,10 +81,11 @@ plot_mean_sd(fit_aghq, fit_nuts, "Besag model")
 ggsave("besag-aghq-nuts.png", h = 3.5, w = 6.25)
 
 time_df <- rbind(time_df, c("Besag", get_time(time_aghq), get_time(time_nuts)))
+diagnostics_df <- rbind(diagnostics_df, c("Besag", min(head(summary(fit_nuts)$summary[, "n_eff"], -1)), max(head(bayesplot::rhat(fit_nuts), -1))))
 
 #' BYM2
 tictoc::tic()
-fit_aghq <- arealutils::bym2_aghq(sf, k = 3)
+fit_aghq <- arealutils::bym2_aghq(sf, k = 3, ii = NULL)
 time_aghq <- tictoc::toc()
 
 tictoc::tic()
@@ -93,24 +96,26 @@ plot_mean_sd(fit_aghq, fit_nuts, "BYM2 model")
 ggsave("bym2-aghq-nuts.png", h = 3.5, w = 6.25)
 
 time_df <- rbind(time_df, c("BYM2", get_time(time_aghq), get_time(time_nuts)))
+diagnostics_df <- rbind(diagnostics_df, c("BYM2", min(head(summary(fit_nuts)$summary[, "n_eff"], -1)), max(head(bayesplot::rhat(fit_nuts), -1))))
 
-#' #' FCK
-#' tictoc::tic()
-#' fit_aghq <- arealutils::fck_aghq(sf, k = 3)
-#' time_aghq <- tictoc::toc()
-#'
-#' tictoc::tic()
-#' fit_nuts <- arealutils::fck_tmbstan(sf, nsim_warm = 500, nsim_iter = 1000, chains = 4)
-#' time_nuts <- tictoc::toc()
-#'
-#' plot_mean_sd(fit_aghq, fit_nuts, "FCK model")
-#' ggsave("fck-aghq-nuts.png", h = 3.5, w = 6.25)
-#'
-#' time_df <- rbind(time_df, c("FCK", get_time(time_aghq), get_time(time_nuts)))
-#'
+#' FCK
+tictoc::tic()
+fit_aghq <- arealutils::fck_aghq(sf, k = 3, ii = NULL)
+time_aghq <- tictoc::toc()
+
+tictoc::tic()
+fit_nuts <- arealutils::fck_tmbstan(sf, nsim_warm = 500, nsim_iter = 1000, chains = 4)
+time_nuts <- tictoc::toc()
+
+plot_mean_sd(fit_aghq, fit_nuts, "FCK model")
+ggsave("fck-aghq-nuts.png", h = 3.5, w = 6.25)
+
+time_df <- rbind(time_df, c("FCK", get_time(time_aghq), get_time(time_nuts)))
+diagnostics_df <- rbind(diagnostics_df, c("FCK", min(head(summary(fit_nuts)$summary[, "n_eff"], -1)), max(head(bayesplot::rhat(fit_nuts), -1))))
+
 #' #' FIK
 #' tictoc::tic()
-#' fit_aghq <- arealutils::fik_aghq(sf, k = 3)
+#' fit_aghq <- arealutils::fik_aghq(sf, k = 3, ii = NULL)
 #' time_aghq <- tictoc::toc()
 #'
 #' tictoc::tic()
@@ -121,10 +126,11 @@ time_df <- rbind(time_df, c("BYM2", get_time(time_aghq), get_time(time_nuts)))
 #' ggsave("fik-aghq-nuts.png", h = 3.5, w = 6.25)
 #'
 #' time_df <- rbind(time_df, c("FIK", get_time(time_aghq), get_time(time_nuts)))
+#' diagnostics_df <- rbind(diagnostics_df, c("FIK", min(head(summary(fit_nuts)$summary[, "n_eff"], -1)), max(head(bayesplot::rhat(fit_nuts), -1))))
 #'
 #' #' CK
 #' tictoc::tic()
-#' fit_aghq <- arealutils::ck_aghq(sf, k = 3)
+#' fit_aghq <- arealutils::ck_aghq(sf, k = 3, ii = NULL)
 #' time_aghq <- tictoc::toc()
 #'
 #' tictoc::tic()
@@ -136,7 +142,7 @@ time_df <- rbind(time_df, c("BYM2", get_time(time_aghq), get_time(time_nuts)))
 #'
 #' time_df <- rbind(time_df, c("CK", get_time(time_aghq), get_time(time_nuts)))
 
-names(time_df) <- c("inf_model", "aghq", "tmbstan")
+readr::write_csv(diagnostics_df, "diagnostics.csv")
 readr::write_csv(time_df, "time.csv")
 
 time_df %>%
@@ -147,11 +153,11 @@ time_df %>%
   ) %>%
   mutate(
     time = as.numeric(time),
-    inf_model = forcats::fct_relevel(inf_model, "IID", "Besag", "BYM2")
+    inf_model = forcats::fct_relevel(inf_model, "IID", "Besag", "BYM2", "FCK", "FIK", "CK")
   ) %>%
   ggplot(aes(x = inf_model, y = time, fill = software)) +
     geom_col(position = position_dodge(), width = 0.5) +
-    scale_fill_manual(values = c("#E69F00", "#F0E442")) +
+    scale_fill_manual(values = c("#D55E00", "#CC79A7")) +
     coord_flip() +
     labs(y = "Time taken (s)", x = "Inferential model", fill = "Software") +
     theme_minimal()
