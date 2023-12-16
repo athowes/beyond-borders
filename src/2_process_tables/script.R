be2e5e63e2d0a9db2b40d1ea3baa8f6c68d462c8
athowes <adamthowes@gmail.com>
2023-12-16 11:46:58 +0000

    Add MSE CV table

--- src/2_process_tables/script.R
+++ src/2_process_tables/script.R
@@ -15,7 +15,7 @@ df <- bind_rows(
   discard_fails(readRDS("depends/cv_ck.rds"))
 )
 
-df_crps <- df %>%
+df <- df %>%
   mutate(
     type = toupper(type),
     inf_model = recode_factor(
@@ -36,7 +36,9 @@ df_crps <- df %>%
       "tza2017phia" = "Tanzania, PHIA 2017",
       "zwe2016phia" = "Zimbabwe, PHIA 2016"
     )
-  ) %>%
+  )
+
+df_crps <- df %>%
   group_by(inf_model, type, survey) %>%
   filter(!is.na(crps)) %>%
   summarise(
@@ -45,12 +47,41 @@ df_crps <- df %>%
   )
 
 gt_crps <- df_crps %>%
-  mutate(value = paste0(10^3 * round(mean, digits = 4), " (", 10^3 * round(se, digits = 4), ")")) %>%
+  mutate(mean = 1000 * mean, se = 1000 * se) %>%
+  mutate(value = paste0(sprintf("%0.1f", mean), " (", sprintf("%0.1f", se), ")")) %>%
   select(Survey = survey, inf_model, value) %>%
   spread(inf_model, value) %>%
   gt() %>%
   cols_align(align = "left", columns = "Survey") %>%
   tab_spanner(label = "Continuous ranked probability score (units: 1/1000)", columns = -Survey) %>%
-  sub_missing(missing_text = "-")
+  sub_missing(missing_text = "-") %>%
+  tab_style(
+    style = cell_text(align = "center"),
+    locations = cells_column_labels(columns = -Survey)
+  )
 
 saveRDS(gt_crps, "gt_cv-crps.rds")
+
+df_mse <- df %>%
+  group_by(inf_model, type, survey) %>%
+  filter(!is.na(mse)) %>%
+  summarise(
+    mean = mean(mse),
+    se = sd(mse) / n()
+  )
+
+gt_mse <- df_mse %>%
+  mutate(mean = 1000 * mean, se = 1000 * se) %>%
+  mutate(value = paste0(sprintf("%0.02f", mean), " (", ifelse(round(se, 2) == 0, "<0.01", sprintf("%0.2f", se)), ")")) %>%
+  select(Survey = survey, inf_model, value) %>%
+  spread(inf_model, value) %>%
+  gt() %>%
+  cols_align(align = "left", columns = "Survey") %>%
+  tab_spanner(label = "Mean squared error (units: 1/1000)", columns = -Survey) %>%
+  sub_missing(missing_text = "-") %>%
+  tab_style(
+    style = cell_text(align = "center"),
+    locations = cells_column_labels(columns = -Survey)
+  )
+
+saveRDS(gt_mse, "gt_cv-mse.rds")
