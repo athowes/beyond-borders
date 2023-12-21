d533134ac8baee11b38f38be22fedc3d9715b05b
athowes <adamthowes@gmail.com>
2023-12-21 12:55:53 +0000

    Add BYM2 proportion posteriors for surveys

--- src/2_plot_hypers/script.R
+++ src/2_plot_hypers/script.R
@@ -34,7 +34,39 @@ bym2_index <- key_df %>%
   pull(id) %>%
   as.numeric()
 
-summary(ic_fit[[bym2_index[1]]])
+phi_samples <- lapply(bym2_index, function(i) {
+  fit <- ic_fit[[i]]
+  samples <- aghq::sample_marginal(fit, M = 1000)
+  theta_samples <- data.frame(do.call(cbind, samples$thetasamples))
+  names(theta_samples) <- names(fit$optresults$mode)
+  phi <- plogis(theta_samples$logit_phi)
+  survey_id <- filter(key_df, id == i)$survey_id
+  data.frame(phi = phi, survey_id = survey_id)
+})
+
+bind_rows(phi_samples) %>%
+  mutate(
+    survey_id = forcats::fct_recode(survey_id,
+      "Côte d’Ivoire, PHIA 2017" = "CIV2017PHIA",
+      "Malawi, PHIA 2016" = "MWI2016PHIA",
+      "Tanzania, PHIA 2017" = "TZA2017PHIA",
+      "Zimbabwe, PHIA 2016" = "ZWE2016PHIA",
+    )
+  ) %>%
+  ggplot() +
+    geom_histogram(aes(x = phi, y = after_stat(density)), fill = "#56B4E9") +
+    stat_function(fun = dbeta, colour = "#009E73", n = 300, args = list(shape1 = 0.5, shape2 = 0.5)) +
+    facet_wrap(~ survey_id, ncol = 2) +
+    xlim(0, 1) +
+    labs(x = "BYM2 proportion parameter", y = "Density", fill = "") +
+    geom_col(data = data.frame(x = c(0, 0), y = c(0, 0), type = c("Prior", "Posterior")), aes(x = x, y = y, fill = type)) +
+    scale_fill_manual(values = c("#56B4E9", "#009E73")) +
+    theme_minimal() +
+    theme(
+      legend.position = "right"
+    )
+
+ggsave("proportion-posteriors.png", h = 3.5, w = 6.25, bg = "white")
 
 #' Lengthscale
 lengthscale_index <- key_df %>%
