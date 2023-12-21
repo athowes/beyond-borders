860c74981d98d4386fec55b1983d5e0f1b6071b0
athowes <adamthowes@gmail.com>
2023-12-21 13:28:06 +0000

    Add lengthscale posteriors (need to add priors, though a little difficult as they depend on the geometry)

--- src/2_plot_hypers/script.R
+++ src/2_plot_hypers/script.R
@@ -58,7 +58,7 @@ bind_rows(phi_samples) %>%
     stat_function(fun = dbeta, colour = "#009E73", n = 300, args = list(shape1 = 0.5, shape2 = 0.5)) +
     facet_wrap(~ survey_id, ncol = 2) +
     xlim(0, 1) +
-    labs(x = "BYM2 proportion parameter", y = "Density", fill = "") +
+    labs(x = "BYM2 proportion", y = "Density", fill = "") +
     geom_col(data = data.frame(x = c(0, 0), y = c(0, 0), type = c("Prior", "Posterior")), aes(x = x, y = y, fill = type)) +
     scale_fill_manual(values = c("#56B4E9", "#009E73")) +
     theme_minimal() +
@@ -74,7 +74,34 @@ lengthscale_index <- key_df %>%
   pull(id) %>%
   as.numeric()
 
-summary(ic_fit[[lengthscale_index[1]]])
+l_samples <- lapply(lengthscale_index, function(i) {
+  fit <- ic_fit[[i]]
+  samples <- aghq::sample_marginal(fit, M = 1000)
+  theta_samples <- data.frame(do.call(cbind, samples$thetasamples))
+  names(theta_samples) <- names(fit$optresults$mode)
+  l <- exp(theta_samples$log_l)
+  survey_id <- filter(key_df, id == i)$survey_id
+  data.frame(l = l, survey_id = survey_id)
+})
 
-exp(-1.4)
-exp(2.7)
+bind_rows(l_samples) %>%
+  mutate(
+    survey_id = forcats::fct_recode(survey_id,
+      "Côte d’Ivoire, PHIA 2017" = "CIV2017PHIA",
+      "Malawi, PHIA 2016" = "MWI2016PHIA",
+      "Tanzania, PHIA 2017" = "TZA2017PHIA",
+      "Zimbabwe, PHIA 2016" = "ZWE2016PHIA",
+    )
+  ) %>%
+  ggplot() +
+  geom_histogram(aes(x = l, y = after_stat(density)), fill = "#56B4E9") +
+  facet_wrap(~ survey_id, ncol = 2) +
+  labs(x = "Lengthscale", y = "Density", fill = "") +
+  geom_col(data = data.frame(x = c(0, 0), y = c(0, 0), type = c("Prior", "Posterior")), aes(x = x, y = y, fill = type)) +
+  scale_fill_manual(values = c("#56B4E9", "#009E73")) +
+  theme_minimal() +
+  theme(
+    legend.position = "right"
+  )
+
+ggsave("lengthscale-posteriors.png", h = 3.5, w = 6.25, bg = "white")
