f2b3d9798a649fb19587dab5ee410a2f9b3b06a2
athowes <adamthowes@gmail.com>
2023-12-28 15:01:37 +0000

    Check-in on rerunning simulation study with 200 draws from each marginal

--- make/1_all.R
+++ make/1_all.R
@@ -4,14 +4,13 @@ source("make/utils.R")
 run_commit_push("1_sim_geometries", param = list(nsim = 250)) #' [x]
 
 #' 2. Run models and assess
-run_commit_push("1_run", param = list(f = "constant_aghq", nsim = 250)) #' [ ]
-run_commit_push("1_run", param = list(f = "iid_aghq", nsim = 250))      #' [x] 1 hour
-run_commit_push("1_run", param = list(f = "besag_aghq", nsim = 250))    #' [x] 2 hours
-run_commit_push("1_run", param = list(f = "bym2_aghq", nsim = 250))     #' [x] 2 hours
-run_commit_push("1_run", param = list(f = "fck_aghq", nsim = 250))      #' [x] 15 minutes
-run_commit_push("1_run", param = list(f = "fik_aghq", nsim = 250))      #' [x] 30 minutes
-run_commit_push("1_run", param = list(f = "ck_aghq", nsim = 250))       #' [x] 30 minutes (this seems puzzlingly low)
-run_commit_push("1_run", param = list(f = "ik_aghq", nsim = 250))       #' [x] 3.5 hours
+run_commit_push("1_run", param = list(f = "iid_aghq", nsim = 250))      #' [x] 30 minutes
+run_commit_push("1_run", param = list(f = "besag_aghq", nsim = 250))    #' [ ] 2 hours
+run_commit_push("1_run", param = list(f = "bym2_aghq", nsim = 250))     #' [ ] 2.5 hours
+run_commit_push("1_run", param = list(f = "fck_aghq", nsim = 250))      #' [x] 30 minutes
+run_commit_push("1_run", param = list(f = "fik_aghq", nsim = 250))      #' [x] 40 minutes
+run_commit_push("1_run", param = list(f = "ck_aghq", nsim = 250))       #' [x] 40 minutes
+run_commit_push("1_run", param = list(f = "ik_aghq", nsim = 250))       #' [ ] 3.6 hours
 
 #' 3. Post-process
 run_commit_push("1_process_tables")    #' [x]
